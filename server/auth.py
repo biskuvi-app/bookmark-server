167776f05b38251798acc420f0e9c2ff0f970368
Qatoqat <qatoqatt@gmail.com>
2024-11-19 18:19:54 +0800

    undo temp jwt

--- server/auth.py
+++ server/auth.py
@@ -1,5 +1,3 @@
-import logging
-
 import atproto
 from atproto import DidInMemoryCache, IdResolver, verify_jwt
 from atproto.exceptions import TokenInvalidSignatureError
@@ -24,10 +22,8 @@ def validate_auth(request: 'Request') -> str:
     jwt = auth_header[len('Bearer '):].strip()
 
     try:
-        # return verify_jwt(jwt, _ID_RESOLVER.did.resolve_atproto_key).iss
-        payload = atproto.decode_jwt_payload(jwt)
-        did = payload.iss
-        logging.info(did)
-        return did
+        return verify_jwt(jwt, _ID_RESOLVER.did.resolve_atproto_key).iss
+        # payload = atproto.decode_jwt_payload(jwt)
+        # return payload
     except TokenInvalidSignatureError as e:
         raise AuthorizationError('Invalid signature') from e
