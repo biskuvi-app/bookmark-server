66a0f9147bdea650362fa9ce8e2b53c4555e100a
Qatoqat <qatoqatt@gmail.com>
2024-11-19 17:42:54 +0800

    iss

--- server/auth.py
+++ server/auth.py
@@ -1,3 +1,6 @@
+import logging
+
+import atproto
 from atproto import DidInMemoryCache, IdResolver, verify_jwt
 from atproto.exceptions import TokenInvalidSignatureError
 from flask import Request
@@ -21,6 +24,10 @@ def validate_auth(request: 'Request') -> str:
     jwt = auth_header[len('Bearer '):].strip()
 
     try:
-        return verify_jwt(jwt, _ID_RESOLVER.did.resolve_atproto_key).iss
+        # return verify_jwt(jwt, _ID_RESOLVER.did.resolve_atproto_key).iss
+        payload = atproto.decode_jwt_payload(jwt)
+        did = payload.iss
+        logging.info(did)
+        return did
     except TokenInvalidSignatureError as e:
         raise AuthorizationError('Invalid signature') from e
