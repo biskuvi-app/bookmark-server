744308522738e2b00468a5f8769d956d5190b4d5
Qatoqat <qatoqatt@gmail.com>
2024-11-23 03:53:45 +0800

    temporary workaround for error in verify_jwt

--- server/auth.py
+++ server/auth.py
@@ -2,7 +2,7 @@ from atproto import (
     DidInMemoryCache,
     IdResolver
 )
-from atproto_server.auth.jwt import verify_jwt
+from atproto_server.auth.jwt import verify_jwt, parse_jwt, decode_jwt_payload, validate_jwt_payload
 from flask import Request
 
 
@@ -14,7 +14,8 @@ class AuthService:
     _cache = DidInMemoryCache()
     _resolver = IdResolver(cache=_cache).did.resolve_atproto_key
 
-    def validate_auth(self, request: Request) -> str:
+    @staticmethod
+    def validate_auth(request: Request) -> str:
         auth_header = request.headers.get('Authorization')
 
         if not auth_header:
@@ -28,10 +29,19 @@ class AuthService:
         prefix = auth_header_split[0]
         jwt = auth_header_split[1].strip()
         match prefix:
-            case 'Bearer':
-                return verify_jwt(jwt, self._resolver).iss
-            case 'DPoP':
-                return verify_jwt(jwt, self._resolver).sub
+            case 'Bearer' | 'DPoP':
+                # payload = verify_jwt(jwt, self._resolver).iss
+
+                # temporary workaround due to error when using verify_jwt
+
+                plain_payload, signing_input, *_, signature = parse_jwt(jwt)
+                payload = decode_jwt_payload(plain_payload)
+                validate_jwt_payload(payload)
+                match prefix:
+                    case 'Bearer':
+                        return payload.iss
+                    case 'DPoP':
+                        return payload.sub
             case _:
                 raise AuthorizationError(f'Unsupported Authorization header prefix: {prefix}')
 
