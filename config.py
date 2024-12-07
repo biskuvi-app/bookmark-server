fd9c467f9ea5635e92ba3104c5d32681ffba1c5d
Qatoqat <qatoqatt@gmail.com>
2024-12-07 15:29:42 +0800

    plc to web in config

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 class Config:
     HOSTNAME = "bookmarks.bskv.site"
     SERVICE_DID = "did:plc:qvmvynssslo5yhstrnc2cwv6"
-    BOOKMARKS_URI = f"at://{SERVICE_DID}/app.bsky.feed.generator/bookmarks"
+    BOOKMARKS_URI = f"at://did:web:bookmarks.bskv.site/app.bsky.feed.generator/bookmarks"
     GIT_REPO_URL = "https://biskuvi-app.github.io"
     BOOKMARKS_DIR = "repo_bookmarks"
     LOGS_DIR = "logs"
